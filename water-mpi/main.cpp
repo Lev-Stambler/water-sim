47e3798b4b880edc854d4e2e51d3753362f067f9
Lev Stambler <levstamb@gmail.com>
2021-04-25 13:46:36 -0400

    add synch

--- water-mpi/main.cpp
+++ water-mpi/main.cpp
@@ -1,15 +1,14 @@
+#include "mpi.h"
 #include <assert.h>
-#include <stdio.h>
-#include <time.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <stdint.h>
 #include <error.h>
 #include <limits.h>
 #include <stdarg.h>
-#include "mpi.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #define MPI_CELL_DATA_TYPE MPI_INT
 typedef int CellData;
@@ -18,41 +17,90 @@ int nproc;
 const int NUMB_BOXES_X = 3;
 const int NUMB_BOXES_Y = 3;
 const int NUMB_BOXES = NUMB_BOXES_X * NUMB_BOXES_Y;
-const int BOX_WIDTH = 100;
-const int BOX_HEIGHT = 100;
-CellData * boxData;
+const int BOX_WIDTH = 101;
+const int BOX_HEIGHT = 101;
+CellData *boxData;
 int boxID;
 int xBoxPos;
 int yBoxPos;
 
-
-
 void computeSection();
 
 /**
-* @brief Synchronize the edges of each section across the different CUDA devices
-*/
+ * @brief Synchronize the edges of each section across the different CUDA
+ * devices
+ */
 void sync_info() {
   // TODO: impl
   /*
-    This can be synchronous with each frame. Then, it could be made potentially faster by synchronizing only with its neighbours
-    So, it would like have to wait on its neighbour to get data and continue and its neighbour would have to weight on this to get data and continue. Then, ig how would 
-    you either avoid locking (where both are synchronous w/o a set order) or avoid one section going a frame ahead and then giving the wrong data to its neighbours.
+    This can be synchronous with each frame. Then, it could be made potentially
+    faster by synchronizing only with its neighbours So, it would like have to
+    wait on its neighbour to get data and continue and its neighbour would have
+    to weight on this to get data and continue. Then, ig how would you either
+    avoid locking (where both are synchronous w/o a set order) or avoid one
+    section going a frame ahead and then giving the wrong data to its
+    neighbours.
   */
 }
 
-static void send_async_to_box(int recieverID, CellData * data, int count, MPI_Request *req) {
-  MPI_Isend(data, count, MPI_CELL_DATA_TYPE, recieverID, 0, MPI_COMM_WORLD, req);
+static void send_async_to_box(int recieverID, CellData *data, int count,
+                              MPI_Request *req) {
+  MPI_Isend(data, count, MPI_CELL_DATA_TYPE, recieverID, 0, MPI_COMM_WORLD,
+            req);
 }
 
-static int calc_id(int x, int y) {
-  return x + y * NUMB_BOXES_X;
-}
+static int calc_id(int x, int y) { return x + y * NUMB_BOXES_X; }
 
-static void get_box_col(CellData * col, int colNumb) {
+static void set_box_col(CellData *col, int colNumb) {
+  for (size_t i = 0; i < BOX_HEIGHT; i++) {
+    boxData[i * BOX_WIDTH + colNumb] = col[i];
+  }
+}
+static void get_box_col(CellData *col, int colNumb) {
   for (size_t i = 0; i < BOX_HEIGHT; i++) {
     col[i] = boxData[i * BOX_WIDTH + colNumb];
   }
+}
+
+static void receive_data(int senderID, CellData *data, int count) {
+  MPI_Recv(data, count, MPI_CELL_DATA_TYPE, senderID, 0, MPI_COMM_WORLD,
+           MPI_STATUS_IGNORE);
+}
+
+// TODO: does sending over the 0th and last of every bottom/ top/ left/ right
+// boundary matter?
+void update_curr_boundary() {
+
+  CellData *boxBottomBoundaryData = &boxData[(BOX_HEIGHT - 1) * BOX_WIDTH];
+  CellData *boxTopBoundaryData = &boxData[(BOX_HEIGHT - 1) * BOX_WIDTH];
+  CellData boxLeftBoundaryData[BOX_HEIGHT];
+  CellData boxRightBoundaryData[BOX_HEIGHT];
+
+  if (NUMB_BOXES_Y > 1) {
+    if (yBoxPos == 0) {
+      receive_data(calc_id(xBoxPos, yBoxPos + 1), boxBottomBoundaryData, BOX_WIDTH);
+    } else if (yBoxPos < NUMB_BOXES_Y - 1) {
+      receive_data(calc_id(xBoxPos, yBoxPos - 1), boxTopBoundaryData, BOX_WIDTH);
+      receive_data(calc_id(xBoxPos, yBoxPos + 1), boxBottomBoundaryData, BOX_WIDTH);
+    } else {
+      receive_data(calc_id(xBoxPos, yBoxPos - 1), boxTopBoundaryData, BOX_WIDTH);
+    }
+  }
+  if (NUMB_BOXES_X > 0) {
+    if (xBoxPos == 0) {
+      receive_data(calc_id(xBoxPos + 1, yBoxPos), boxRightBoundaryData, BOX_WIDTH);
+      set_box_col(boxRightBoundaryData, BOX_WIDTH - 1);
+    } else if (xBoxPos < NUMB_BOXES_X - 1) {
+      receive_data(calc_id(xBoxPos + 1, yBoxPos), boxRightBoundaryData, BOX_WIDTH);
+      receive_data(calc_id(xBoxPos - 1, yBoxPos), boxLeftBoundaryData, BOX_WIDTH);
+      set_box_col(boxRightBoundaryData, BOX_WIDTH - 1);
+      set_box_col(boxLeftBoundaryData, 0);
+    } else {
+      receive_data(calc_id(xBoxPos - 1, yBoxPos), boxLeftBoundaryData, BOX_WIDTH);
+      set_box_col(boxLeftBoundaryData, 0);
+    }
+  }
+
 }
 
 void send_boundry_px_msg() {
@@ -61,40 +109,48 @@ void send_boundry_px_msg() {
   MPI_Request west_req;
   MPI_Request east_req;
 
-  CellData * boxBottomData = &boxData[(BOX_HEIGHT - 1) * BOX_WIDTH];
-  CellData * boxTopData = &boxData[0];
+  CellData *boxBottomData = &boxData[(BOX_HEIGHT - 2) * BOX_WIDTH];
+  CellData *boxTopData = &boxData[1];
   CellData boxLeftData[BOX_HEIGHT];
   CellData boxRightData[BOX_HEIGHT];
-  get_box_col(boxLeftData, 0);
-  get_box_col(boxRightData, BOX_WIDTH - 1);
+  get_box_col(boxLeftData, 1);
+  get_box_col(boxRightData, BOX_WIDTH - 2);
 
-
-  if (yBoxPos == 0 && NUMB_BOXES_Y > 1) {
-    send_async_to_box(calc_id(xBoxPos, yBoxPos + 1), boxBottomData, BOX_WIDTH, &south_req);
-  } else if (yBoxPos < NUMB_BOXES_Y - 1) {
-    send_async_to_box(calc_id(xBoxPos, yBoxPos - 1), boxTopData, BOX_WIDTH, &north_req);
-    send_async_to_box(calc_id(xBoxPos, yBoxPos + 1), boxBottomData, BOX_WIDTH, &south_req);
-  } else {
-    // Send to the noth
-    send_async_to_box(calc_id(xBoxPos, yBoxPos - 1), boxTopData, BOX_WIDTH, &north_req);
+  if (NUMB_BOXES_Y > 1) {
+    if (yBoxPos == 0) {
+      send_async_to_box(calc_id(xBoxPos, yBoxPos + 1), boxBottomData, BOX_WIDTH,
+                        &south_req);
+    } else if (yBoxPos < NUMB_BOXES_Y - 1) {
+      send_async_to_box(calc_id(xBoxPos, yBoxPos - 1), boxTopData, BOX_WIDTH,
+                        &north_req);
+      send_async_to_box(calc_id(xBoxPos, yBoxPos + 1), boxBottomData, BOX_WIDTH,
+                        &south_req);
+    } else {
+      // Send to the noth
+      send_async_to_box(calc_id(xBoxPos, yBoxPos - 1), boxTopData, BOX_WIDTH,
+                        &north_req);
+    }
   }
-  if (xBoxPos == 0) {
-    send_async_to_box(calc_id(xBoxPos + 1, yBoxPos), boxRightData, BOX_WIDTH, &east_req);
-  } else if (xBoxPos < NUMB_BOXES_X - 1) {
-    send_async_to_box(calc_id(xBoxPos + 1, yBoxPos), boxRightData, BOX_WIDTH, &east_req);
-    send_async_to_box(calc_id(xBoxPos - 1, yBoxPos), boxLeftData, BOX_WIDTH, &west_req);
-  } else {
-    send_async_to_box(calc_id(xBoxPos - 1, yBoxPos), boxLeftData, BOX_WIDTH, &west_req);
+  if (NUMB_BOXES_X > 0) {
+    if (xBoxPos == 0) {
+      send_async_to_box(calc_id(xBoxPos + 1, yBoxPos), boxRightData, BOX_WIDTH,
+                        &east_req);
+    } else if (xBoxPos < NUMB_BOXES_X - 1) {
+      send_async_to_box(calc_id(xBoxPos + 1, yBoxPos), boxRightData, BOX_WIDTH,
+                        &east_req);
+      send_async_to_box(calc_id(xBoxPos - 1, yBoxPos), boxLeftData, BOX_WIDTH,
+                        &west_req);
+    } else {
+      send_async_to_box(calc_id(xBoxPos - 1, yBoxPos), boxLeftData, BOX_WIDTH,
+                        &west_req);
+    }
   }
 }
 
 /**
-* @brief Run the cuda section to compute the process current section
-*/
-void run_cuda() {
-  computeSection();
-}
-
+ * @brief Run the cuda section to compute the process current section
+ */
+void run_cuda() { computeSection(); }
 
 // static void error_exit(const char * f, ...) {
 //   va_list args;
