9ff136f59f0d2be90da347c21b5afe272df3fcba
Lev Stambler <levstamb@gmail.com>
2021-05-07 11:07:42 -0400

    stuff

--- water-mpi/run.sh
+++ water-mpi/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mpirun --plm_rsh_agent badSSH:ssh:rsh --mca oob_tcp_static_ports 22 -np $1 --hostfile hostfile watermpi 
+mpirun --mca plm_rsh_agent badSSH.sh:ssh:rsh oob_tcp_static_ports 22 -np $1 --hostfile hostfile watermpi 
 # mpirun -np $1 --hostfile hostfile watermpi
