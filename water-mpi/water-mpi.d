c89765b121ff5625e5645788ee0e863bb095a18e
Lev David Stambler <lstamble@ghc50.ghc.andrew.cmu.edu>
2021-05-11 13:56:20 -0400

    exec

---
+++ water-mpi/water-mpi.d
@@ -0,0 +1,6 @@
+water-mpi.o: water-mpi.c /usr/local/depot/openmpi-4.0.2/include/mpi.h \
+ /usr/local/depot/openmpi-4.0.2/include/mpi_portable_platform.h
+
+/usr/local/depot/openmpi-4.0.2/include/mpi.h:
+
+/usr/local/depot/openmpi-4.0.2/include/mpi_portable_platform.h:
